d96dedb617dc7328ed95f1286f898bd023827305
FredrikOseberg <fredrik.no@gmail.com>
2018-03-14 21:24:55 +0100

    eslint fix

--- src/app/store/configureStore.dev.js
+++ src/app/store/configureStore.dev.js
@@ -1,7 +1,9 @@
 import { applyMiddleware, createStore, compose } from 'redux'
+import { persistState } from 'redux-devtools'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 import storage from '../utils/storage'
+import DevTools from '../containers/DevTools'
 import createHistory from 'history/createBrowserHistory'
 import { routerMiddleware } from 'react-router-redux'
 
@@ -16,7 +18,18 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   : compose
 /* eslint-enable no-underscore-dangle */
 
-const enhancer = composeEnhancers(applyMiddleware(...[thunk, routerMiddleware(history)]), storage())
+const enhancer = composeEnhancers(
+  applyMiddleware(...[thunk, routerMiddleware(history)]),
+  storage(),
+  DevTools.instrument(),
+  persistState(getDebugSessionKey())
+)
+
+function getDebugSessionKey() {
+  // By default we try to read the key from ?debug_session=<key> in the address bar
+  const matches = window.location.href.match(/[?&]debug_session=([^&#]+)\b/)
+  return matches && matches.length > 0 ? matches[1] : null
+}
 
 export default function(initialState) {
   if (initialState.router && initialState.router.location) {
