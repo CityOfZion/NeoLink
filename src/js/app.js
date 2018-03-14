740881952ef33dfbc600fc5ea1980bf9eeea2bac
FredrikOseberg <fredrik.no@gmail.com>
2018-03-14 21:21:07 +0100

    Extract fontawesome to vendor folder

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from '../app/Root'
-import './fontawesome-all.min.js'
+import '../../vendor/fontawesome-all.min'
 
 chrome.storage.local.get('state', obj => {
   const { state } = obj
