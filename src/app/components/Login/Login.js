2e851e9a486768eefc8d01630c7324f36c913939
FredrikOseberg <fredrik.no@gmail.com>
2018-03-15 19:57:41 +0100

    Delete and recreate folder

--- src/app/components/Login/Login.js
+++ src/app/components/Login/Login.js
@@ -12,7 +12,9 @@ import '@material/textfield/dist/mdc.textfield.min.css'
 import '@material/select/dist/mdc.select.min.css'
 
 import Loader from '../Loader'
-import StartPage from '../StartPage/index'
+import StartPage from '../StartPage'
+
+console.log(StartPage)
 
 export class Login extends Component {
   state = {
