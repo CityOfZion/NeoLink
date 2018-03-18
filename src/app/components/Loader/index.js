d9679dedf952b4c10037814e5f1064a3fe2893a4
FredrikOseberg <fredrik.no@gmail.com>
2018-03-18 21:48:02 +0100

    Remove loader heading

--- src/app/components/Loader/index.js
+++ src/app/components/Loader/index.js
@@ -4,7 +4,6 @@ import style from './Loader.css'
 
 const Loader = () => (
   <section className={ style.loaderWrapper }>
-    <h2 className={ style.loaderWrapperHeading }>Preparing to enter the matrix.</h2>
     <div className={ style.loader } />
   </section>
 )
