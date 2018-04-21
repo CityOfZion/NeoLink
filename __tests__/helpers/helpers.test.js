cfbd41d401179c446421831b1f10d9b7c99ff141
FredrikOseberg <fredrik.no@gmail.com>
2018-04-21 15:06:57 +0200

    Comment out integration tests. Getting reworked in another PR

--- __tests__/helpers/helpers.test.js
+++ __tests__/helpers/helpers.test.js
@@ -13,36 +13,36 @@ const networks = {
   TestNet: { name: 'TestNet', url: 'https://neoscan-testnet.io/api/test_net', canDelete: false, apiType: 'neoscan' },
 }
 
-test('getBalance gets the correct balance', () => {
-  return getBalance(networks, network, account).then(data => {
-    expect(data.neo).toBe(1)
-    expect(data.gas).toBe(0.2)
-  })
-})
+// test('getBalance gets the correct balance', () => {
+//   return getBalance(networks, network, account).then(data => {
+//     expect(data.neo).toBe(1)
+//     expect(data.gas).toBe(0.2)
+//   })
+// })
 
-test('getBalance works with neonDB', () => {
-  const networks = {
-    CoZTestNet: {
-      name: 'CoZ TestNet',
-      url: 'http://coz-privatenet.herokuapp.com/',
-      canDelete: false,
-      apiType: 'neonDB',
-    },
-    MainNet: { name: 'MainNet', url: 'http://api.wallet.cityofzion.io', canDelete: false, apiType: 'neonDB' },
-    TestNet: { name: 'TestNet', url: 'http://testnet-api.wallet.cityofzion.io', canDelete: false, apiType: 'neonDB' },
-  }
+// test('getBalance works with neonDB', () => {
+//   const networks = {
+//     CoZTestNet: {
+//       name: 'CoZ TestNet',
+//       url: 'http://coz-privatenet.herokuapp.com/',
+//       canDelete: false,
+//       apiType: 'neonDB',
+//     },
+//     MainNet: { name: 'MainNet', url: 'http://api.wallet.cityofzion.io', canDelete: false, apiType: 'neonDB' },
+//     TestNet: { name: 'TestNet', url: 'http://testnet-api.wallet.cityofzion.io', canDelete: false, apiType: 'neonDB' },
+//   }
 
-  return getBalance(networks, network, account).then(data => {
-    expect(data.neo).toBe(1)
-    expect(data.gas).toBe(0.2)
-  })
-})
+//   return getBalance(networks, network, account).then(data => {
+//     expect(data.neo).toBe(1)
+//     expect(data.gas).toBe(0.2)
+//   })
+// })
 
-test('getTransactions gets the correct number of transactions', () => {
-  return getTransactions(networks, network, account).then(data => {
-    expect(data.length).toBe(2)
-  })
-})
+// test('getTransactions gets the correct number of transactions', () => {
+//   return getTransactions(networks, network, account).then(data => {
+//     expect(data.length).toBe(2)
+//   })
+// })
 
 test('formatGas formats gas under 1 correctly', () => {
   const formattedGas = formatGas([0, 1323242])
