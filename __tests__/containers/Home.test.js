f88687accbc810a0d66e4565a9ad903f23724fd9
FredrikOseberg <fredrik.no@gmail.com>
2018-04-24 22:40:30 +0200

    Finish refactoring tests for getBalance and getTransaction in Home

--- __tests__/containers/Home.test.js
+++ __tests__/containers/Home.test.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { mount, shallow } from 'enzyme'
 import MockStore from '../../__mocks__/MockStore'
 import { StaticRouter } from 'react-router'
+import * as helpers from '../../src/app/utils/helpers'
 
 import Home from '../../src/app/containers/Home/Home'
 
@@ -25,6 +26,9 @@ describe('Home', () => {
         key: '6PYRop1b45uKRUVUngUr3g44UmH8Kg6KTVTAvxyKKJLVpxQsM5HXUPrzCB',
         label: 'TestKonto',
       },
+    },
+    networks: {
+      TestNetworks: 'TestNetworks',
     },
   }
 
@@ -62,17 +66,20 @@ describe('Home', () => {
   })
 
   test('calls getBalance and getTransactions correctly', () => {
-    const getBalance = jest.fn()
-    const getTransactions = jest.fn()
+    helpers.getBalance = jest.fn(() => new Promise((resolve, reject) => resolve()))
+    helpers.getTransactions = jest.fn(() => new Promise((resolve, reject) => resolve()))
 
-    const wrapper = shallow(<Home { ...props } getBalance={ getBalance } getTransactions={ getTransactions } />)
+    shallow(<Home { ...props } />)
 
-    wrapper.instance().getHomeScreenBalance = jest.fn()
-    wrapper.instance().getHomeScreenTransactions = jest.fn()
-
-    wrapper.instance().componentDidMount()
-
-    expect(getBalance).toHaveBeenCalledTimes(1)
-    expect(getTransactions).toHaveBeenCalledTimes(1)
+    expect(helpers.getBalance).toHaveBeenCalledTimes(1)
+    expect(helpers.getTransactions).toHaveBeenCalledTimes(1)
+    expect(helpers.getBalance).toHaveBeenCalledWith({ TestNetworks: 'TestNetworks' }, 'TestNet', {
+      address: 'ARjkxk6VcKPFKqRHhuLNog9TbdYxhKu9be',
+      wif: 'KxyKz2LaFSCi2UQtpxnXs3jdzE5uAxguBRSgbiXMi6adkbivt2ub',
+    })
+    expect(helpers.getTransactions).toHaveBeenCalledWith({ TestNetworks: 'TestNetworks' }, 'TestNet', {
+      address: 'ARjkxk6VcKPFKqRHhuLNog9TbdYxhKu9be',
+      wif: 'KxyKz2LaFSCi2UQtpxnXs3jdzE5uAxguBRSgbiXMi6adkbivt2ub',
+    })
   })
 })
