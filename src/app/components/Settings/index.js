c3fff9a1d0213b8fa627ed030a95749e22c9145e
FredrikOseberg <fredrik.no@gmail.com>
2018-04-28 14:49:51 +0200

    Add settings page

---
+++ src/app/components/Settings/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from './Settings.css'
+
+const Settings = () => (
+  <section className={ style.settings }>
+    <h1>Settings</h1>
+  </section>
+)
+
+export default Settings
