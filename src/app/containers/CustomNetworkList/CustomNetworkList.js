5ba78e853cdb9a8a462887b4a3a91ac7dd7157b6
FredrikOseberg <fredrik.no@gmail.com>
2018-05-06 13:23:55 +0200

    Custom network list initial styling

--- src/app/containers/CustomNetworkList/CustomNetworkList.js
+++ src/app/containers/CustomNetworkList/CustomNetworkList.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 
+import SettingsNavigation from '../../components/SettingsNavigation'
+
 import style from './CustomNetworkList.css'
+import Settings from '../../components/Settings'
 
 class CustomNetworkList extends Component {
-  delete = (index) => {
+  delete = index => {
     const { deleteCustomNetwork, setNetwork, selectedNetworkId } = this.props
 
     // If this is the current network they're using, reset to MainNet
@@ -17,23 +20,23 @@ class CustomNetworkList extends Component {
 
   generateNetworkRows(networks) {
     const networkRows = []
-    Object.keys(networks).forEach((index) => {
+    Object.keys(networks).forEach(index => {
       const network = networks[index]
       if (network.canDelete) {
-        networkRows.push((
-          <div key={ `network-item-${index}` } className={ style.networkWrapper }>
-            <div className={ style.networkInfo }>
-              <div>{ network.name }</div>
-              <div>Endpoint: { network.url }</div>
+        networkRows.push(
+          <section className={ style.customNetworkCard }>
+            <div className={ style.customNetworkColorContainer }>
+              <div className={ style.customNetworkColor } />
             </div>
-            <a className={ style.deleteIcon } onClick={ () => this.delete(index) }>
-              <svg fill='red' height='18' viewBox='0 0 24 24' width='18' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z' />
-                <path d='M0 0h24v24H0z' fill='none' />
-              </svg>
-            </a>
-          </div>
-        ))
+            <div className={ style.customNetworkContainer }>
+              <h3>{network.name}</h3>
+              <h3>{network.url}</h3>
+            </div>
+            <button onClick={ () => this.delete(index) } className={ style.tempButton }>
+              <i className='fas fa-ellipsis-v' />
+            </button>
+          </section>
+        )
       }
     })
 
@@ -41,16 +44,20 @@ class CustomNetworkList extends Component {
   }
 
   render() {
-    const { networks } = this.props
+    const { networks, history } = this.props
 
     const networkRows = this.generateNetworkRows(networks)
 
-    const content = networkRows.length ? <div>{ networkRows }</div> : 'You have no custom networks defined'
+    const content = networkRows.length ? <Fragment>{networkRows}</Fragment> : 'You have no custom networks defined'
 
     return (
-      <div>
-        {content}
-      </div>
+      <Fragment>
+        <SettingsNavigation history={ history } />
+        <section className={ style.manageNetworksContainer }>
+          <h1>Manage Networks</h1>
+          {content}
+        </section>
+      </Fragment>
     )
   }
 }
@@ -60,6 +67,7 @@ CustomNetworkList.propTypes = {
   deleteCustomNetwork: PropTypes.func.isRequired,
   setNetwork: PropTypes.func.isRequired,
   selectedNetworkId: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
 }
 
 export default CustomNetworkList
